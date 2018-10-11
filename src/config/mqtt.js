c17407ce8904406630ad74d58fcfcb2ab3767ec5
Julien Clément <julienclement22@gmail.com>
2018-10-11 19:53:03 +0200

    MQTT Broker configuration TG-19 #in-progress

---
+++ src/config/mqtt.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = {
+  mqttuser: process.env.MQTT_USER || 'blue',
+  mqttpassword: process.env.MQTT_PASSWORD || 'f58e6c045b',
+  wsshost: process.env.MQTT_WSS_HOST || 'mqtt.thing.zone:8094',
+  mqtthost: process.env.MQTT_MQTT_HOST || 'mqtt.thing.zone:1894'
+};
+
+export default config;
