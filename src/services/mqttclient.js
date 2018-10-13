4b6739532d7855dd3a84ba3ebee8694763eff8f2
Thomas Schaller <thomasschaller647@gmail.com>
2018-10-13 14:36:26 +0200

    add crlf to lf in gitattributes

--- src/services/mqttclient.js
+++ src/services/mqttclient.js
@@ -1,13 +1,13 @@
 'use strict';
 
 import mqtt from 'mqtt';
-import config from '../config/mqtt.js'
+import config from '../config/mqtt.js';
 import logger from '../logger';
-import thingy from '../config/thingy.js'
+import thingy from '../config/thingy.js';
 
 const mqttclient = () => {
   // Connect to the MQTT broker
-  const client = mqtt.connect(config.wsshost, {username: config.mqttuser, password: config.mqttpassword});
+  const client = mqtt.connect(config.wsshost, { username: config.mqttuser, password: config.mqttpassword });
 
   // Subscribe to everything
   // Will change once we store the UUIDs of the Thingys
@@ -18,43 +18,43 @@ const mqttclient = () => {
   client.on('message', (topic, message) => {
     let thingyURI = topic.substr(0, topic.indexOf('/'));
 
-    //temperature
-    if(topic.toString().includes(thingy.characteristics.temperature.characteristicUUID)){
+    // temperature
+    if(topic.toString().includes(thingy.characteristics.temperature.characteristicUUID)) {
       let value = message[0] + '.' + message[1];
-      logger.info({ event: 'mqtt'}, 'Thingy ' + thingyURI + ' -> Temperature: '+ value + thingy.characteristics.temperature.unit);
+      logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Temperature: ' + value + thingy.characteristics.temperature.unit);
     }
 
     // pressure
-    else if(topic.toString().includes(thingy.characteristics.pressure.characteristicUUID)){
+    else if(topic.toString().includes(thingy.characteristics.pressure.characteristicUUID)) {
       // TODO
     }
 
     // humidity
-    else if(topic.toString().includes(thingy.characteristics.humidity.characteristicUUID)){
+    else if(topic.toString().includes(thingy.characteristics.humidity.characteristicUUID)) {
       let value = message[0];
-      logger.info({ event: 'mqtt'}, 'Thingy ' + thingyURI + ' -> Humidity: '+ value + thingy.characteristics.humidity.unit);
+      logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Humidity: ' + value + thingy.characteristics.humidity.unit);
     }
 
     // gas
-    else if(topic.toString().includes(thingy.characteristics.gaseco2.characteristicUUID)){
+    else if(topic.toString().includes(thingy.characteristics.gaseco2.characteristicUUID)) {
       // TODO
     }
 
     // light
-    else if(topic.toString().includes(thingy.characteristics.light.characteristicUUID)){
+    else if(topic.toString().includes(thingy.characteristics.light.characteristicUUID)) {
       // TODO
     }
 
     // button
-    else if(topic.toString().includes(thingy.characteristics.button.characteristicUUID)){
+    else if(topic.toString().includes(thingy.characteristics.button.characteristicUUID)) {
       let value = (message[0] == 0x00) ? 'released' : 'pressed';
-      logger.info({ event: 'mqtt'}, 'Thingy ' + thingyURI + ' -> Button '+ value);
+      logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Button ' + value);
     }
   });
 
   return async (ctx, next) => {
     await next();
   };
-}
+};
 
 export default mqttclient;
