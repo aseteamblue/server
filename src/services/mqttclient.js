4987a19335ed7356172a6a7958817808c0f833cf
Julien Clément <julienclement22@gmail.com>
2018-10-13 18:51:30 +0200

    Starts getting the data only when the user presses the button of his Thingy. Stops when he presses it again. Only supports a unique Thingy for the moment. Now supports gas, pressure, GPS, temperature, button and humidity. Formatted the code correctly. TG-15 TG-17 TG-19 #ready-for-test

--- src/services/mqttclient.js
+++ src/services/mqttclient.js
@@ -5,7 +5,7 @@ import config from '../config/mqtt.js';
 import logger from '../logger';
 import thingy from '../config/thingy.js';
 
-const mqttclient = () => {
+const mqttclient = () =>{
   // Connect to the MQTT broker
   const client = mqtt.connect(config.wsshost, { username: config.mqttuser, password: config.mqttpassword });
 
@@ -15,45 +15,47 @@ const mqttclient = () => {
   // -> subscribe only when the user presses the button
   client.subscribe('#');
 
+  // Only here to test
+  // We'll need to store that. If (receive){user is running -> get data from broker} else {user is not running -> do nothing}
+  let receive = new Map();
+
   client.on('message', (topic, message) => {
     let thingyURI = topic.substr(0, topic.indexOf('/'));
-
-    // temperature
-    if(topic.toString().includes(thingy.characteristics.temperature.characteristicUUID)) {
-      let value = message[0] + '.' + message[1];
-      logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Temperature: ' + value + thingy.characteristics.temperature.unit);
+    if(!receive.has(thingyURI)) {
+      receive.set(thingyURI, false);
     }
 
-    // pressure
-    else if(topic.toString().includes(thingy.characteristics.pressure.characteristicUUID)) {
-      // TODO
-    }
-
-    // humidity
-    else if(topic.toString().includes(thingy.characteristics.humidity.characteristicUUID)) {
-      let value = message[0];
-      logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Humidity: ' + value + thingy.characteristics.humidity.unit);
-    }
-
-    // gas
-    else if(topic.toString().includes(thingy.characteristics.gaseco2.characteristicUUID)) {
-      // TODO
-    }
-
-    // light
-    else if(topic.toString().includes(thingy.characteristics.light.characteristicUUID)) {
-      // TODO
-    }
-
-    // button
-    else if(topic.toString().includes(thingy.characteristics.button.characteristicUUID)) {
-      let value = (message[0] == 0x00) ? 'released' : 'pressed';
+    if(topic.toString().includes(thingy.characteristics.button.characteristicUUID)) {
+      // button
+      let value = (message[0] === 0x00) ? 'released' : 'pressed';
       logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Button ' + value);
+      if (message[0] === 0x0) { receive.set(thingyURI, !receive.get(thingyURI)); }
     }
 
-    // GPS position
-    else if(topic.toString().includes("gps")){
-      logger.info({event:'mqtt'}, 'Position:'+ message);
+    if(receive.get(thingyURI)) {
+      if(topic.toString().includes(thingy.characteristics.temperature.characteristicUUID)) {
+        // temperature
+        let value = message[0] + '.' + message[1];
+        logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Temperature: ' + value + thingy.characteristics.temperature.unit);
+      } else if(topic.toString().includes(thingy.characteristics.pressure.characteristicUUID)) {
+        // pressure
+        let intBinary = message[3].toString(2) + message[2].toString(2) + message[1].toString(2) + message[0].toString(2);
+        let value = parseFloat(parseInt(intBinary, 2) + '.' + message[4]);
+        logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Pressure: ' + value + thingy.characteristics.pressure.unit);
+      } else if(topic.toString().includes(thingy.characteristics.humidity.characteristicUUID)) {
+        // humidity
+        let value = message[0];
+        logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Humidity: ' + value + thingy.characteristics.humidity.unit);
+      } else if(topic.toString().includes(thingy.characteristics.gaseco2.characteristicUUID)) {
+        // gas
+        let eco2 = parseInt(message[1].toString(2) + message[0].toString(2), 2);
+        let tvoc = parseInt(message[3].toString(2) + message[2].toString(2), 2);
+        logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> Gas (eCO2): ' + eco2 + thingy.characteristics.gaseco2.unit + ', Gas (TVOC): ' + tvoc + thingy.characteristics.gastvoc.unit);
+      } else if(topic.toString().includes('gps')) {
+        // GPS
+        let value = JSON.parse(message);
+        logger.info({ event: 'mqtt' }, 'Thingy ' + thingyURI + ' -> GPS: ' + value.latitude + ', ' + value.longitude);
+      }
     }
   });
 
