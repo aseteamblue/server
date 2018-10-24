fabb7513c92dff6ed6bae54c7a87e87fed470941
Romain Maillard <romain.flac@gmail.com>
2018-10-24 17:02:35 +0200

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -7,11 +7,7 @@ const UserSchema = new Schema({
   'firstName': { type: String },
   'address': { type: String },
   'username': { type: String },
-<<<<<<< HEAD
   'password': { type: String },
-=======
-  'password' : { type: String },
->>>>>>> 3d2b8e1ed8761fbd75867e19be4ca8df9a8621f8
   'totalDistanceJogging': { type: Number },
   'totalDistanceBicycle': { type: Number },
   'totalDistance': { type: Number },
